058f6f7cf39f7cb32cc6e3b50d5617a6ec884637
EzEmbedded <EzEmbedded@users.noreply.github.com>
2021-12-29 12:38:07 +0800

    Update install-libreelec-need-pakages.sh

--- scripts/install-libreelec-need-pakages.sh
+++ scripts/install-libreelec-need-pakages.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 sudo -E apt-get -qq update
 sudo apt-get upgrade
-sudo -E apt-get install gcc make git unzip wget xz-utils bc gperf zip unzip makeinfo g++ mkfontscale mkfontdir bdftopcf xsltproc openjdk-8-jdk texinfo ttf-mscorefonts-installer fontconfig xfonts-utils xsltproc libparse-yapp-perl lzop patchutils 
+sudo apt-get install ttf-mscorefonts-installer
+sudo apt-get install fontconfig
+sudo -E apt-get install gcc make git unzip wget xz-utils bc gperf zip unzip makeinfo g++ mkfontscale mkfontdir bdftopcf xsltproc openjdk-8-jdk texinfo  xfonts-utils xsltproc libparse-yapp-perl lzop patchutils 
 sudo -E apt-get -qq autoremove --purge
 sudo -E apt-get -qq clean
 sudo timedatectl set-timezone "$TZ"
