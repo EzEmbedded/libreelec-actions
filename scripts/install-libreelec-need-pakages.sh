555381011915b1e5de0d09d92cebf14e0b32a61f
EzEmbedded <EzEmbedded@users.noreply.github.com>
2021-12-29 11:12:18 +0800

    Update install-libreelec-need-pakages.sh

--- scripts/install-libreelec-need-pakages.sh
+++ scripts/install-libreelec-need-pakages.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo -E apt-get -qq update
-sudo -E apt-get install gcc make git unzip wget xz-utils bc gperf zip unzip makeinfo g++ mkfontscale mkfontdir bdftopcf xsltproc java xfonts-utils xsltproc libparse-yapp-perl gperf lzop patchutils 
+sudo apt-get upgrade
+sudo -E apt-get install gcc make git unzip wget xz-utils bc gperf zip unzip makeinfo g++ mkfontscale mkfontdir bdftopcf xsltproc java xfonts-utils xsltproc libparse-yapp-perl lzop patchutils 
 sudo -E apt-get -qq autoremove --purge
 sudo -E apt-get -qq clean
 sudo timedatectl set-timezone "$TZ"
