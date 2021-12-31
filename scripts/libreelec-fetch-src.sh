44931ef8ec38d9a9f58016181c648a0b62ac6239
EzEmbedded <EzEmbedded@users.noreply.github.com>
2021-12-31 13:50:47 +0800

    Update libreelec-fetch-src.sh

--- scripts/libreelec-fetch-src.sh
+++ scripts/libreelec-fetch-src.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-git clone https://github.com/LibreELEC/LibreELEC.tv.git
-git checkout 10.0.0
+git clone https://github.com/LibreELEC/LibreELEC.tv.git -b 10.0.0
 cd LibreELEC.tv
 pwd 
 ls -al
