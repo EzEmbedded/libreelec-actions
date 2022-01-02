99d62d65b5f1a72802d714760fbd915f7f977775
EzEmbedded <EzEmbedded@users.noreply.github.com>
2022-01-02 19:35:35 +0800

    Update libreelec-docker.sh

--- scripts/libreelec-docker.sh
+++ scripts/libreelec-docker.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-git clone https://github.com/LibreELEC/LibreELEC.tv.git -b libreelec-10.0
+git clone https://github.com/LibreELEC/LibreELEC.tv.git
 cd LibreELEC.tv
 docker build --pull -t libreelec tools/docker/focal
+git clone https://github.com/LibreELEC/LibreELEC.tv.git -b libreelec-10.0
+cd LibreELEC.tv
 docker images
 pwd
 sudo chmod -R  777 ./
