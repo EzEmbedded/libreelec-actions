7c8fb8b1950164ca142be20b80d83dcb03bc0204
EzEmbedded <EzEmbedded@users.noreply.github.com>
2022-01-02 21:42:37 +0800

    Update libreelec-docker.sh

--- scripts/libreelec-docker.sh
+++ scripts/libreelec-docker.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-git clone https://github.com/LibreELEC/LibreELEC.tv.git
+git clone https://github.com/LibreELEC/LibreELEC.tv.git  -b libreelec-10.0
 cd LibreELEC.tv
 docker build --pull -t libreelec tools/docker/focal
-git clone https://github.com/LibreELEC/LibreELEC.tv.git -b libreelec-10.0
-cd LibreELEC.tv
+
 docker images
 pwd
 sudo chmod -R  777 ./
 # sudo mkdir -p /build/build.LibreELEC-AMLGX.arm-10.0-devel  && sudo -E chmod 777 /build/build.LibreELEC-AMLGX.arm-10.0-devel
-
+docker run --rm -v `pwd`:/build -w /build -i -e PROJECT=Amlogic -e DEVICE=AMLGX -e ARCH=arm -e ONELOG=no -e LOGCOMBINE=never libreelec tools/download-tool
 docker run --rm -v `pwd`:/build -w /build -i -e PROJECT=Amlogic -e DEVICE=AMLGX -e ARCH=arm -e ONELOG=no -e LOGCOMBINE=never libreelec make image
 df -h
+
