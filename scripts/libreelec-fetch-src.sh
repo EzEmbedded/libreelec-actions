a60cabf988277165da832b556294b139ba01b867
EzEmbedded <EzEmbedded@users.noreply.github.com>
2022-02-08 14:04:04 +0800

    last

--- scripts/libreelec-fetch-src.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git clone https://github.com/LibreELEC/LibreELEC.tv.git -b libreelec-10.0
-cd LibreELEC.tv
-pwd 
-ls -al
-PROJECT=Amlogic ARCH=arm DEVICE=AMLGX ONELOG=no LOGCOMBINE=fail tools/download-tool
-
-PROJECT=Amlogic ARCH=arm DEVICE=AMLGX ONELOG=no LOGCOMBINE=never THREADCOUNT=50% make image 
